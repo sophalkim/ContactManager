12578d96c69a3927456a114b8eb6c6ec7c3c2795
Sophal Kim <sophal.kim.work@gmail.com>
2015-04-06 19:36:37 -0700

    Init Repo, had trouble generating controller with entity framework

---
+++ ContactManager/Models/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ContactManager.Models
+{
+    public class Contact
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+
+    public class ContactDBContext : DbContext
+    {
+        public DbSet<Contact> Movies { get; set; }
+    }
+}
